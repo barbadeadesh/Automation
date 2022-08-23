373f50fa40a5ad1d75cdd737e905587f90f6ed61
barbadeadesh <barbadeadesh2020@email.com>
2022-08-23 13:03:05 +0530

    ADDED: Functionality added for fb login by using css selector

---
+++ Csss_selector.java
@@ -0,0 +1,25 @@
+package selenium.automation107;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class facebook_Css_SelectorTest {
+
+	@Test
+	public void css() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Selenium Automation\\automation107\\Driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/login/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("input[id='email']")).sendKeys("adeshbarbade7@gmail.com");
+		driver.findElement(By.cssSelector("input[id='pass']")).sendKeys("Adesh@8888073562@gmail.com");
+		driver.findElement(By.cssSelector("button[id='loginbutton']")).click();
+		Thread.sleep(2000);
+		driver.close();
+	}
+}
